c124633a4f9daf62df4942f6c4a566bc2eb4b891
AyanaHirai <115526744+AyanaHirai@users.noreply.github.com>
2023-01-05 00:59:33 +0900

    Add files via upload

---
+++ gs_php_kiroku/update.php
@@ -0,0 +1,43 @@
+<?php
+
+//PHP:コード記述/修正の流れ
+//1. insert.phpの処理をマルっとコピー。
+//2. $id = $_POST["id"]を追加
+//3. SQL修正
+//   "UPDATE テーブル名 SET 変更したいカラムを並べる WHERE 条件"
+//   bindValueにも「id」の項目を追加
+//4. header関数"Location"を「select.php」に変更
+//1. POSTデータ取得
+$name   = $_POST['name'];
+$email  = $_POST['email'];
+$title    = $_POST['title'];
+$content = $_POST['content'];
+$id = $_POST['id'];
+
+//2. DB接続します
+require_once('funcs.php');
+$pdo = db_conn();
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare('UPDATE gs_bm_table SET name = :name, email = :email, title = :title, content = :content, indate = sysdate() WHERE id = :id;');
+
+// 数値の場合 PDO::PARAM_INT
+// 文字の場合 PDO::PARAM_STR
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT); //PARAM_INTなので注意
+
+$status = $stmt->execute(); //実行
+
+//４．データ登録処理後
+if ($status === false) {
+    //*** function化する！******\
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+} else {
+    //*** function化する！*****************
+    header('Location: select.php');
+    exit();
+}
